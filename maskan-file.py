cae9868222ce017b4da60cc4ea69576b67755501
ILIA <ih13843251@gmail.com>
2025-05-14 01:02:19 -0700

    maskan-file scraper pictures fix

--- maskan-file.py
+++ maskan-file.py
@@ -1,10 +1,11 @@
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time  
 
 class RealEstateScraper:
     def __init__(self, property_code):
         self.property_code = property_code
-        self.base_url = "https://maskan-file.ir//Site/Homes.aspx?codeFile="
+        self.base_url = "https://maskan-file.ir/Site/Homes.aspx?codeFile=" 
         self.data = {
             "file_code": property_code,
             "title": "",
@@ -16,7 +17,7 @@ class RealEstateScraper:
             "area": "",
             "number_of_rooms": "",
             "year_of_manufacture": "",
-            "facilities": "",
+            "facilities": [],
             "pictures": [],
             "is_rental": False
         }
@@ -24,9 +25,19 @@ class RealEstateScraper:
     def scrape(self):
         try:
             url = self.base_url + str(self.property_code)
-            response = requests.get(url)
-            response.raise_for_status()  
-            soup = BeautifulSoup(response.text, 'html.parser')
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless') 
+            options.add_argument('--disable-gpu')
+            options.add_argument('--no-sandbox')
+            
+            driver = webdriver.Chrome(options=options)
+            driver.get(url)
+            
+            
+            time.sleep(2)
+            
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            driver.quit()  
 
             property_type_div = soup.select_one('div.col-md-4.col-sm-4.col-lg-3.col-xs-12.col-12')
             if property_type_div and "رهن و اجاره" in property_type_div.get_text(strip=True):
@@ -36,11 +47,11 @@ class RealEstateScraper:
             self._extract_address(soup)
             self._extract_pricing_info(soup)
             self._extract_property_details(soup)
-#            self.data["pictures"] = self._scrape_images(soup)
+            self.data["pictures"] = self._scrape_images(soup)
 
             return self.data
 
-        except requests.exceptions.RequestException as e:
+        except Exception as e:
             print(f"Error fetching data: {e}")
             return None
 
@@ -53,14 +64,21 @@ class RealEstateScraper:
 
     def _extract_pricing_info(self, soup):
         if self.data["is_rental"]:
-            self.data["mortgage"] = self._extract_text(soup, 'div.col-md-2.col-sm-2.col-lg-5.card-body.ForPrint h3')
-            self.data["rent"] = self._extract_text(soup, 'div.col-md-2.col-sm-2.col-lg-5.card-body.ForPrint h5 span')
+            mortgage_element = soup.select_one('div.col-md-2.col-sm-2.col-lg-5.card-body.ForPrint h3')
+            self.data["mortgage"] = mortgage_element.get_text(strip=True) if mortgage_element else ""
+            
+            rent_element = soup.select_one('div.col-md-2.col-sm-2.col-lg-5.card-body.ForPrint h5 span')
+            self.data["rent"] = rent_element.get_text(strip=True) if rent_element else ""
         else:
-            self.data["total_price"] = self._extract_text(soup, 'div.card-body h4')
-            self.data["price_per_meter"] = self._extract_text(soup, 'div.col-md-6.col-sm-6.col-lg-6.col-xs-12 > span.spanMatns')
+            total_price_element = soup.select_one('div.card-body h4')
+            self.data["total_price"] = total_price_element.get_text(strip=True) if total_price_element else ""
+            
+            price_per_meter_element = soup.select_one('div.col-md-6.col-sm-6.col-lg-6.col-xs-12 > span.spanMatns')
+            self.data["price_per_meter"] = price_per_meter_element.get_text(strip=True) if price_per_meter_element else ""
 
     def _extract_property_details(self, soup):
-        self.data["area"] = self._extract_text(soup, 'div.Metrazh.matns2 span.matns2')
+        area_element = soup.select_one('div.Metrazh.matns2 span.matns2')
+        self.data["area"] = area_element.get_text(strip=True) if area_element else ""
         
         rooms_div = soup.select_one('div.col-md-4.col-sm-4.col-lg-4.col-xs-12:-soup-contains("تعداد خواب")')
         self.data["number_of_rooms"] = self._extract_text(rooms_div, 'span.spanMatns').strip() if rooms_div else ""
@@ -69,24 +87,29 @@ class RealEstateScraper:
         self.data["year_of_manufacture"] = self._extract_text(year_div, 'span.spanMatns').strip() if year_div else ""
         
         facilities_div = soup.select_one('div.Facilities')
-        self.data["facilities"] = [item.get_text(strip=True) for item in facilities_div.select('li.lis')] if facilities_div else []
+        if facilities_div:
+            self.data["facilities"] = [item.get_text(strip=True) for item in facilities_div.select('li.lis')]
+        else:
+            self.data["facilities"] = []
 
     def _extract_text(self, parent, selector):
+        if parent is None:
+            return ""
         element = parent.select_one(selector)
         return element.get_text(strip=True) if element else ""
 
-#    def _scrape_images(self, soup):
+    def _scrape_images(self, soup):
         try:
             all_images = set()
+            main_images = soup.select('div.mySlides img[src]')
             
-            main_images = soup.select('div.mySlides img.imgslides[src]')
-            thumb_images = soup.select('img.demo[src]')
-            
-            for img in main_images + thumb_images:
-                src = img['src']
-                if 'index.png' not in src:
+            for img in main_images:
+                src = img.get('src', '')
+                if 'index.png' not in src and src:
                     if src.startswith('../../../../../'):
                         src = "https://maskan-file.ir" + src.replace('../../../../../', '/')
+                    elif not src.startswith(('http://', 'https://')):
+                        src = "https://maskan-file.ir" + src if src.startswith('/') else f"https://maskan-file.ir/{src}"
                     all_images.add(src)
             
             return list(all_images)
@@ -96,6 +119,7 @@ class RealEstateScraper:
             return []
 
 if __name__ == "__main__":
-    scraper = RealEstateScraper(input())
+    scraper = RealEstateScraper(input("Enter property code: "))
     property_data = scraper.scrape()
     print(property_data if property_data else "Failed to retrieve property data")
+    

142ee2d9345a572b87694a94d11af6ea8cdd9275
Homaun <houmanhoseinpour@gmail.com>
2025-05-17 12:46:42 +0330

    add table for similarity results

--- database_manager.py
+++ database_manager.py
@@ -34,6 +34,13 @@ class Data(Base):
     pictures = Column(JSON, nullable=True)
     is_rental = Column(Boolean, nullable=True)
 
+class Similarity(Base):
+    __tablename__ = "similarity"
+    id = Column(Integer, primary_key=True, autoincrement=True)  # Unique ID
+    id_1 = Column(Integer, nullable=True)
+    id_2 = Column(Integer, nullable=True)
+    similarity = Column(Float, nullable=True)
+ 
 
 # Create all tables
 Base.metadata.create_all(engine)
@@ -57,25 +64,40 @@ def session_scope():
 
 # Function to create data
 def create_data(dict_data):
+    # if 
+        try:
+            with session_scope() as session:
+                new_data = Data(
+                    file_code=dict_data.get("file_code", ""),
+                    title=dict_data.get("title", ""),
+                    address=dict_data.get("address", ""),
+                    total_price=dict_data.get("total_price"),
+                    price_per_meter=dict_data.get("price_per_meter"),
+                    mortgage=dict_data.get("mortgage"),
+                    rent=dict_data.get("rent"),
+                    area=dict_data.get("area"),
+                    number_of_rooms=dict_data.get("number_of_rooms"),
+                    year_of_manufacture=dict_data.get("year_of_manufacture"),
+                    facilities=dict_data.get("facilities", []), 
+                    pictures=dict_data.get("pictures", []),  
+                    is_rental=dict_data.get("is_rental")
+                )
+                session.add(new_data)
+                logging.info(f"Inserted data: {dict_data.get('file_code')}")
+        except SQLAlchemyError as e:
+            logging.error(f"Error inserting data: {e}")
+
+# Function to create similarity data
+def create_sim(dict_sim):
     try:
         with session_scope() as session:
-            new_data = Data(
-                file_code=dict_data.get("file_code", ""),
-                title=dict_data.get("title", ""),
-                address=dict_data.get("address", ""),
-                total_price=dict_data.get("total_price"),
-                price_per_meter=dict_data.get("price_per_meter"),
-                mortgage=dict_data.get("mortgage"),
-                rent=dict_data.get("rent"),
-                area=dict_data.get("area"),
-                number_of_rooms=dict_data.get("number_of_rooms"),
-                year_of_manufacture=dict_data.get("year_of_manufacture"),
-                facilities=dict_data.get("facilities", []), 
-                pictures=dict_data.get("pictures", []),  
-                is_rental=dict_data.get("is_rental")
+            new_sim_data = Similarity(
+                id_1 = dict_sim.get("property_1"),
+                id_2 = dict_sim.get("property_2"),
+                similarity = dict_sim.get("similarity")
             )
-            session.add(new_data)
-            logging.info(f"Inserted data: {dict_data.get('file_code')}")
+            session.add(new_sim_data)
+            logging.info(f"Inserted data: {dict_sim.get('property_1')} and {dict_sim.get('property_2')}")
     except SQLAlchemyError as e:
         logging.error(f"Error inserting data: {e}")
 
@@ -155,11 +177,20 @@ if __name__ == "__main__":
          }
     ]
 
-    # Insert test data
-    for data in test_data:
-        create_data(data)
+    sim = [
+        {
+            'property_1' : 1,
+            'property_2' : 2,
+            'similarity' : 66.6,
+        }
+    ]
+    
 
-    # Fetch and print all data
-    results = select_data()
-    for result in results:
-        print(result)
+    # # Insert test data
+    for data in sim:
+        create_sim(data)
+
+    # # Fetch and print all data
+    # results = select_data()
+    # for result in results:
+    #     print(result)
